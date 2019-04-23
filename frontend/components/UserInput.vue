040c56c9106bc63890a638c170d66f17f2780fbe
Anna Calinawan <anna.pamela@gmail.com>
2019-04-23 16:58:40 -0400

    creates image from container

--- frontend/components/UserInput.vue
+++ frontend/components/UserInput.vue
@@ -150,7 +150,8 @@
                 writeFile(wb, 'CPTAC-CCRCC.xls');
             },
             downloadImage () {
-                let node = document.querySelectorAll('.the-heatmap-container')[0];
+                // let node = document.querySelectorAll('.the-heatmap-container')[0];
+                let node = document.getElementById('the-heatmap-and-legend-container');
                 domtoimage.toPng(node, { quality: 0.95, bgcolor: '#FFFFFF' })
                     .then(function (dataUrl) {
                         let link = document.createElement('a');
@@ -188,9 +189,9 @@
         },
         mounted () {
             this.$store.dispatch('loadFirstData');
-            const defaultGenes = [];
-            // const defaultGenes = ['VHL', 'SETD2', 'PBRM1', 'BAP1', 'NDUFA4L2',
-            //     'VIM', 'ANGPTL4', 'CA9', 'RHCG', 'FOXI1', 'VSTM2A'];
+            // const defaultGenes = [];
+            const defaultGenes = ['VHL', 'SETD2', 'PBRM1', 'BAP1', 'NDUFA4L2',
+                'VIM', 'ANGPTL4', 'CA9', 'RHCG', 'FOXI1', 'VSTM2A'];
             const urlTrail = window.location.href.substr(window.location.href.lastIndexOf('/') + 1);
             let genes = urlTrail.length ? urlTrail.split("&") : defaultGenes;
 
